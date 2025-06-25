7261c45836d62d08eeea46e8ab9c0261fcc39d67
IkumaOka <ikuma.oka@brainpad.co.jp>
2025-06-25 09:49:54 +0900

    add setting from bp mac

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
@@ -8,6 +9,18 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.shiftwidth = 4
     vim.bo.softtabstop = 4
     vim.bo.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   end,
 })
 
