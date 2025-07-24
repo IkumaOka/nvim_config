652cb054a26b26b922df34c61d4bec1dc3a828c9
IkumaOka <ikuma.oka@brainpad.co.jp>
2025-07-24 12:35:03 +0900

    feat: add lsp-config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,28 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        lspconfig.pyright.setup{
-		  lspconfig.pyright.setup {
-            capabilities = capabilities
-          }
-		}
-    end,
+  "neovim/nvim-lspconfig",
+  opts = {},
+  dependencies = {
+   { "mason-org/mason.nvim", version = "^1.0.0" },
+   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+
+    require("mason").setup()
+    local ensure_installed = {'lua_ls', 'pyright', 'ruff', 'rust_analyzer', 'ts_ls', 'jsonls'}
+    require("mason-lspconfig").setup {
+	    automatic_installation = true,
+	    ensure_installed = ensure_installed,
+    }
+    vim.lsp.config('lua_ls', {
+    settings = {
+      Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          },
+        }
+      },
+    })
+    vim.lsp.enable(ensure_installed)
+  end
 }
