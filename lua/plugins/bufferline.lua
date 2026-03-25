5685788e948f9b6b667992a5dec4a0829244445f
IkumaOka <ikuma.oka@brainpad.co.jp>
2026-03-25 17:01:41 +0900

    feat: bufferline.nvim に tab キーで buffer 間を移動する keymap を追加

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,16 +3,19 @@ return {
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
-    	vim.opt.termguicolors = true
-    	require("bufferline").setup{
-		options = {
-			buffer_close_icon = "x",
-			-- nvim-tree を表示している時に上にバッファを表示しないようにする
-			offsets = {
-                           { filetype = "NvimTree" },
-  			},
-		}
-	}
+        vim.opt.termguicolors = true
+        require("bufferline").setup {
+            options = {
+                buffer_close_icon = "x",
+                -- nvim-tree を表示している時に上にバッファを表示しないようにする
+                offsets = {
+                    { filetype = "NvimTree" },
+                },
+            }
+        }
+        -- Tab で右のバッファへ
+        vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { silent = true })
+        -- Shift + Tab で左のバッファへ
+        vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { silent = true })
     end,
 }
-
